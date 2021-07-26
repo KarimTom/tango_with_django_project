a922bbe8200f14e9648ca7b96a6b46f85ba3be19
Karim Altom <karimTom99@hotmail.com>
2021-07-26 21:05:12 +0100

    Did the about request on chap3

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.urls import include
 from rango import views
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('about/', include('about.urls')),
 ]
