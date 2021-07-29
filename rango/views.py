310b870a0597283f05cca5d0b921c7171426ba30
Karim Altom <karimTom99@hotmail.com>
2021-07-29 13:08:17 +0100

    Chapter 8 work

--- rango/views.py
+++ rango/views.py
@@ -19,6 +19,10 @@ def index(request):
 
     return render(request, 'rango/index.html', context = context_dict)
 
+def about(request):
+    context_dict = {'name': 'Karim Altom'}
+    return render(request, 'rango/about.html', context = context_dict)
+
 def show_category(request, category_name_slug):
     context_dict = {}
 
